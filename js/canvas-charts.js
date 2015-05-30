8f34bbbf55780ccd786e24ec3361cbf59e9e3f73
duenni <github@praiseintoxication.eu>
2015-05-30 23:18:41 +0200

    Add canvasjs and implement a static example.

---
+++ js/canvas-charts.js
@@ -0,0 +1,32 @@
+//CanvasJS
+function makeChart() {
+    var chart = new CanvasJS.Chart("chartContainer",
+	{
+		title:{
+			text: "Email Categories",
+			verticalAlign: 'top',
+			horizontalAlign: 'left'
+		},
+                animationEnabled: true,
+		data: [
+		{        
+			type: "doughnut",
+			startAngle:20,
+			toolTipContent: "{label}: {y} - <strong>#percent%</strong>",
+			indexLabel: "{label} #percent%",
+			dataPoints: [
+				{  y: 67, label: "Inbox" },
+				{  y: 28, label: "Archives" },
+				{  y: 10, label: "Labels" },
+				{  y: 7,  label: "Drafts"},
+				{  y: 4,  label: "Trash"}
+			]
+		}
+		]
+	});
+	chart.render();
+}
+$(document).ready(function() {
+    var clickLink = document.getElementById("modal-link");
+    clickLink.addEventListener('click', makeChart);
+});

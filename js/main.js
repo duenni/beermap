2c81a32205acce5a7757d94ee68d28dffe84886c
duenni <github@praiseintoxication.eu>
2015-06-07 12:03:43 +0200

    Integrate canvas-charts.js into main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//-----------------------------Map stuff---------------------------------
 //Initialize map
 var map = new L.map('map', {
     center: [20.0, 5.0],
@@ -16,6 +17,7 @@ var myIcon = L.icon({
 //Copyright information
 map.attributionControl.addAttribution('&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> | <a href="https://www.mapbox.com/map-feedback/">Improve this map</a>');
 
+//-----------------------------EasyButton---------------------------------
 //EasyButton Wiki
 L.easyButton('fa fa-beer', 
     function (){
@@ -39,6 +41,7 @@ L.easyButton('fa fa-bar-chart',
     },
     'More stats'
 )
+//-----------------------------Kimono---------------------------------
 //Use kimonolabs for scraping 
 $.ajax({
     "url":"https://www.kimonolabs.com/api/6qium7f6?apikey="+apikey.kimonolabs,
@@ -68,3 +71,46 @@ $.ajax({
         $( "#sum" ).html( '<i class="fa fa-folder-open">&nbsp;</i>Biere im Wiki: ' + sum );
     }
 }); 
+
+//Modal content
+function makeChart() {  
+    $.ajax({
+        url:"https://www.kimonolabs.com/api/ba3gx8yk?apikey="+apikey.kimonolabs,
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+            //If calling the API was successful create a canvasjs chart
+            var collection = response.results.biersorten;
+            var finals = [];
+            for(var i = 0; i < collection.length; i++)
+            {
+                finals.push({ 'y': parseInt(collection[i].anzahl), 'label': collection[i].sorte.text, 'link': collection[i].sorte.href });
+            }
+                    
+            var chart = new CanvasJS.Chart("chartContainer",{
+                animationEnabled: true,
+                title:{
+                    text: "Biersorten"
+                },
+                
+                data: [
+                {
+                    type: "pie",
+                    toolTipContent: '<a target="_blank" href={link}>{label}</a> <br> Anzahl: {y}',
+                    dataPoints: finals
+                }
+                ]
+            });
+            chart.render();
+        },
+        error: function (xhr, status) {
+            //handle errors
+        }
+    });
+}
+
+//Only render the chart when modal gets clicked
+$(document).ready(function() {
+    var clickLink = document.getElementById("modal-link");
+    clickLink.addEventListener('click', makeChart);
+});

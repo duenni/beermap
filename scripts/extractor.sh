de8497bd550e2fdce4d5e53d019e96075851e181
duenni <github@praiseintoxication.eu>
2017-10-10 17:21:02 +0200

    Fix #24

--- scripts/extractor.sh
+++ scripts/extractor.sh
@@ -4,13 +4,17 @@
 # Run this file in crontab to generate up to date statfiles country.json & style.json in OUTDIR
 
 WGET="/usr/bin/wget -qO-"
-WIKI_PREFIX="http://www.massafaka.at/massawiki"
-WIKI_COUNTRY_URL="${WIKI_PREFIX}/doku.php?id=bierstats:herkunft"
-WIKI_STYLE_URL="${WIKI_PREFIX}/doku.php?id=bierstats:sorten"
+WIKI_PREFIX="http://www.massafaka.at/massawiki/doku.php?id=bierstats"
+WIKI_COUNTRY_URL="${WIKI_PREFIX}:herkunft"
+WIKI_STYLE_URL="${WIKI_PREFIX}:sorten"
+WIKI_OVERVIEW_URL="${WIKI_PREFIX}:uebersicht"
+
 CONTENT_PATTERN="<td class=\"inline\""
 
-OUTDIR="$HOME/html/beermap/js/"
+OUTDIR="$HOME/html/beermap/js"
 FLAG=false
+
+country_beer_counter=0
 
 generate_json ()
 {
@@ -37,6 +41,15 @@ generate_json ()
     printf "\n]" >> ${OUTFILE}
 }
 
-
 generate_json country ${WIKI_COUNTRY_URL}
 generate_json style ${WIKI_STYLE_URL}
+
+
+for i in $(grep anzahl ${OUTDIR}/country.json|cut -d'"' -f4); do let country_beer_counter+=$i; done
+overall_counter=$(${WGET} ${WIKI_OVERVIEW_URL} | grep count overview.out | cut -d'>' -f2 | cut -d'<' -f1)
+if [ ${overall_counter} -ne ${country_beer_counter} ]
+then
+	echo "ERROR: Anzahl Gesamtbiere stimmt nicht mit der \"Herkunfts-Summe\" überein"
+	echo "Anzahl Gesamtbiere       : ${overall_counter}"
+	echo "Anzahl \"Herkunfts-Summe\" : ${country_beer_counter}"
+fi >&2
